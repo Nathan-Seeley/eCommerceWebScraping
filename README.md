a88a9d82cab95c35d1ee3eb5afa134359ea61623
Nathan-Seeley <wntrstk111@gmail.com>
2023-03-07 00:30:11 -0500

    answered questions from user story and saved them on the README.md file

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # eCommerceWebScraping
 
 Q: What was the biggest challenge of scraping the web site?
-A:
+A: 1- Determining if I needed to utilize the wait() function.
+   2- Following the XPATH in order to get the "number of reviews" data.  I lost the trail a couple of times and had to start over.
+   3- Formatting and sorting the .csv file by price.
 
 Q: What insight, if any, did you gain from the analysis and visualization of the laptop data?
-A:
+A: There does not seem to be a strong correlation between the price of the laptop and the number of reviews.
+However, there is a slight trend showing that the higher the price of the item the more reviews there are.
--- computers_laptops.py
+++ computers_laptops.py
@@ -25,8 +25,8 @@ while True:
         name = computer.find_element(By.CLASS_NAME, "title").text
         price = computer.find_element(By.CLASS_NAME, "price").text[1:]
         specifications = computer.find_element(By.CLASS_NAME, "description").text
-        # number_of_reviews = computer.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div/*div[2]/div/div[2]/p[2]").get_attribute("data-rating")
-        number_of_reviews = computer.find_element(By.XPATH,"//p[2]").get_attribute("data-rating")
+        # number_of_reviews = computer.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div/*div[2]/div/div[2]/p[1]").get_attribute("data-rating")
+        number_of_reviews = computer.find_element(By.XPATH,"//div[2]/p[1]").text[0]
         writer.writerow(
             [unique_id, name, price, specifications, number_of_reviews])
         unique_id += 1
