56429cf1fbf12b28a45c3db2f7c1584ad7464021
Nathan-Seeley <wntrstk111@gmail.com>
2023-03-06 21:38:51 -0500

    Finished final scrape to create computers_laptop.csv and adding csv to repository

--- computers_laptops.py
+++ computers_laptops.py
@@ -7,25 +7,26 @@ import csv
 file = open("computers_laptops.csv", "w")
 writer = csv.writer(file)
 writer.writerow(["name", "price","specifications", "number of reviews"])
-browser_driver = Service(r"C:\Users\Owner\Desktop\devCodeCamp projects\Week 17 Web Scraping in Selenium\eCommerce-Project\eCommerceWebScraping\chromedriver.exe")
+browser_driver = Service("chromedriver.exe")
 scraper = webdriver.Chrome(service=browser_driver)
-# scraper = webdriver.ChromeOptions(options=browser_driver)
 scraper.get("https://webscraper.io/test-sites/e-commerce/static/computers/laptops")
-scraper.find_element(By.ID, "closeCookieBanner").click()
+scraper.find_element(By.CLASS_NAME, "acceptCookies").click()
+# wait = WebDriverWait(scraper, 10)
+# element_to_watch = scraper.find_element(By.CLASS_NAME,"col-md-9")
 unique_id = 1
 while True: 
-    computers = scraper.find_elements (By.CLASS_NAME, "pull-right price")
+    computers = scraper.find_elements (By.CLASS_NAME, "thumbnail")
     for computer in computers:
-        name = computer.find_element(By.CLASS_NAME, "title")
-        price = computer.find_element(By.CLASS_NAME, "pull-right price")
-        specifications = computer.find_element(By.CLASS_NAME, "description")
-        number_of_reviews = computer.find_element(By.CLASS_NAME, "pull-right")[0]
+        name = computer.find_element(By.CLASS_NAME, "title").text
+        price = computer.find_element(By.CLASS_NAME, "price").text
+        specifications = computer.find_element(By.CLASS_NAME, "description").text
+        # number_of_reviews = computer.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div/*div[2]/div/div[2]/p[2]").get_attribute("data-rating")
+        number_of_reviews = computer.find_element(By.XPATH,"//p[2]").get_attribute("data-rating")
         writer.writerow(
-            [unique_id, name.text, price.text, specifications.text, number_of_reviews])
+            [unique_id, name, price, specifications, number_of_reviews])
         unique_id += 1
-
     try:
-        element = scraper.find_element(By.PARTIAL_LINK_TEXT, "Next")
+        element = scraper.find_element(By.PARTIAL_LINK_TEXT, "›")
         element.click()
     except NoSuchElementException:
         break
